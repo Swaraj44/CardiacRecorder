784ff0cd11f4800822241312f8d109429d65f636
Swaraj <swarajchbiswas44@gmail.com>
2023-04-20 12:54:39 +0600

    Initial commit in this branch

---
+++ app/src/main/java/com/example/cardiacrecorder/DATA.java
@@ -0,0 +1,53 @@
+package com.example.cardiacrecorder;
+
+public class DATA {
+    private  String date,time,systolic_pressure,diastolic_pressure,heart_rate,comment;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getSystolic_pressure() {
+        return systolic_pressure;
+    }
+
+    public void setSystolic_pressure(String systolic_pressure) {
+        this.systolic_pressure = systolic_pressure;
+    }
+
+    public String getDiastolic_pressure() {
+        return diastolic_pressure;
+    }
+
+    public void setDiastolic_pressure(String diastolic_pressure) {
+        this.diastolic_pressure = diastolic_pressure;
+    }
+
+    public String getHeart_rate() {
+        return heart_rate;
+    }
+
+    public void setHeart_rate(String heart_rate) {
+        this.heart_rate = heart_rate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
