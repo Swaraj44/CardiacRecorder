ce0528b676ae7b15e1cb783210c610f5c2aefaff
Jobair2020 <jobairnahian2017@gmail.com>
2023-07-06 09:49:54 +0600

    UI test

--- app/src/androidTest/java/com/example/cardiacrecorder/ActivityTest.java
+++ app/src/androidTest/java/com/example/cardiacrecorder/ActivityTest.java
@@ -8,6 +8,15 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -22,17 +31,36 @@ import java.util.concurrent.TimeUnit;
 @RunWith(AndroidJUnit4.class)
 public class ActivityTest {
 
-    @Rule
-    public ActivityScenarioRule<Dash_Board> activityRule =
-            new ActivityScenarioRule<>(Dash_Board.class);
+
     @Rule
     public ActivityScenarioRule<MainActivity> loginActivityRule = new ActivityScenarioRule<>(MainActivity.class);
 
+//    @Rule
+//    public ActivityScenarioRule<Dash_Board> activityRule = new ActivityScenarioRule<>(Dash_Board.class);
 
+//    @Test
+//    public void testHeading() {
+//        onView(withText("Add Record")).check(matches(isDisplayed())); //Check the name on the screen
+//    }
 
-    @Test
-    public void testHeading() {
-        onView(withText("Add Record")).check(matches(isDisplayed())); //Check the name on the screen
+    public class RecyclerViewItemCountAssertion implements ViewAssertion {
+        private final int expectedCount;
+
+        public RecyclerViewItemCountAssertion(int expectedCount) {
+            this.expectedCount = expectedCount;
+        }
+
+        @Override
+        public void check(View view, NoMatchingViewException noViewFoundException) {
+            if (noViewFoundException != null) {
+                throw noViewFoundException;
+            }
+
+            RecyclerView recyclerView = (RecyclerView) view;
+            RecyclerView.Adapter adapter = recyclerView.getAdapter();
+//            assert adapter != null;
+            assertThat(adapter.getItemCount(), is(expectedCount));
+        }
     }
 
 
@@ -62,6 +90,83 @@ public class ActivityTest {
 
         onView(withId(R.id.imageSignOut)).check(matches(isDisplayed()));
 
+    }
+
+
+
+
+    @Test
+    public void testAddEntry(){
+
+        try {
+            Thread.sleep(4000); // Adjust the delay as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Create a CountDownLatch with a count of 1
+        final CountDownLatch latch = new CountDownLatch(1);
+
+
+
+        onView(withId(R.id.inputEmail)).perform(ViewActions.typeText("jobairnahian2017@gmail.com"));
+        onView(withId(R.id.inputPassword)).perform(ViewActions.typeText("12345678"));
+        onView(withId(R.id.buttonSignIn)).perform(click());
+
+        // Wait for the RecyclerView action to complete
+        try {
+            latch.await(5, TimeUnit.SECONDS); // Adjust the timeout as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.fabNewEntry)).perform(click());
+        onView(withId(R.id.sp)).perform(ViewActions.typeText("120"));
+        onView(withId(R.id.dp)).perform(ViewActions.typeText("60"));
+        onView(withId(R.id.hr)).perform(ViewActions.typeText("80"));
+
+        Espresso.pressBack();
+        onView(withId(R.id.save_btn)).perform(click());
+
+        try {
+            latch.await(5, TimeUnit.SECONDS); // Adjust the timeout as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        onView(withId(R.id.back_btn)).perform(click());
+        onView(withId(R.id.recordRecyclerView)).check(new RecyclerViewItemCountAssertion(1));
+    }
+
+
+    @Test
+    public void testLogout() {
+        try {
+            Thread.sleep(4000); // Adjust the delay as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Create a CountDownLatch with a count of 1
+        final CountDownLatch latch = new CountDownLatch(1);
+
+
+
+        onView(withId(R.id.inputEmail)).perform(ViewActions.typeText("jobairnahian2017@gmail.com"));
+        onView(withId(R.id.inputPassword)).perform(ViewActions.typeText("12345678"));
+        onView(withId(R.id.buttonSignIn)).perform(click());
+
+        // Wait for the RecyclerView action to complete
+        try {
+            latch.await(5, TimeUnit.SECONDS); // Adjust the timeout as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        onView(withId(R.id.imageSignOut)).perform(click());
+        onView(withId(R.id.welcomeText)).check(matches(isDisplayed()));
     }
 
 }
